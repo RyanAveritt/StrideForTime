ee4272a0981334c01bf4156034d60f56052fccb4
Ryan Averitt <Raveritt@ufl.edu>
2022-11-08 12:00:06 -0500

    added a couple more tests

--- src/calendars/tests.py
+++ src/calendars/tests.py
@@ -15,7 +15,7 @@ class AccountTestCase(TestCase):
     #             location=f'Alachua Habitat for Humanity',
     #             volunteer_type=f'build',
     #         )
-    def test_instance(self):
+    def test_instance_time(self):
         entry1 = Calendar(
             start_time=datetime.datetime(2022, 10, 2, 11, 11, 51),
             end_time=datetime.datetime(2022, 10, 2, 13, 11, 51),
@@ -31,4 +31,22 @@ class AccountTestCase(TestCase):
             volunteer_type=f'build',
         )
         self.assertEqual(str(entry2.duration()), "2 days, 0:00:00")
+
+    def test_type(self):
+        entry = Calendar(
+            start_time=datetime.datetime(2022, 10, 2, 11, 11, 51),
+            end_time=datetime.datetime(2022, 10, 2, 13, 11, 51),
+            location=f'Alachua Habitat for Humanity',
+            volunteer_type=f'build',
+        )
+        self.assertEqual(entry.volunteer_type, "build")
+
+    def test_location(self):
+        entry = Calendar(
+            start_time=datetime.datetime(2022, 10, 2, 11, 11, 51),
+            end_time=datetime.datetime(2022, 10, 2, 13, 11, 51),
+            location=f'Alachua Habitat for Humanity',
+            volunteer_type=f'build',
+        )
+        self.assertEqual(entry.location, "Alachua Habitat for Humanity")
 
